39004af56a8ced14bafeed47cc9e4dfa7a309fea
Niknock HD <87417955+Niknock@users.noreply.github.com>
2024-09-20 23:57:05 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -7,10 +7,14 @@ Config.TargetText = "Take a Donut"
 
 Config.UseNotification = true -- When you get an Donut an Message Appears.
 Config.Notification = 'You got a Donut.'
+Config.NotEnoughMoneyNotification = 'You dont have enought Money.' 
 
 Config.DonutCaseProp = 'v_ret_247_donuts'
 
 Config.DonutItem = 'donut' 
+
+Config.PayForDonut = true   -- Set to false to make the Donut Free
+Config.DonutPrice = 10    -- Price for the Donut
 
 Config.Framework = 'ESX'  -- 'ESX' or 'QBCore'
 Config.ESX = 'New' -- If you use ESX 'New' or 'Old'
